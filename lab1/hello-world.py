71c0b96d68b27d8b6c213ff4fb277a7dad2b1e8a
Sudharshan <sudhar393@gmail.com>
2017-09-04 20:59:10 +0800

    simple hellow world

--- lab1/hello-world.py
+++ lab1/hello-world.py
@@ -5,5 +5,8 @@ input = cv2.imread('../images/input.jpg')
 
 cv2.imshow("Hello World", input)
 
+print("The height of the image is", int(input.shape[0]))
+print("The width of the image is", int(input.shape[1]))
+
 cv2.waitKey()
 cv2.destroyAllWindows()
