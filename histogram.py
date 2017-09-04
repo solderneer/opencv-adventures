a6bd68e8679b75a7a3299db92e9aac448adb3d5d
Sudharshan <sudhar393@gmail.com>
2017-09-04 21:48:31 +0800

    still working on histogram

---
+++ histogram.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+input = cv2.imread('image/input.jpg')
+
+color = ('b','g','r')
+
+for i in enumerate(color):
+    histogram = cv2.calcHist([input], [i], None, [256], [0,256])
+    plt.hist(histogram, color = col)
+    plt.xlim([0,256])
+
+plt.show()
+
