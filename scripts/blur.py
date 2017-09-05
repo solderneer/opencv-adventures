7fcea915bd734f6bec820e1cb687f7a435c3bad6
Sudharshan <sudhar393@gmail.com>
2017-09-05 15:24:59 +0800

    blurring and sharpening again

--- scripts/blur.py
+++ scripts/blur.py
@@ -3,12 +3,25 @@
 import cv2
 import numpy as np
 
-image = imread('../images/input.jpg')
+image = cv2.imread('../images/input.jpg')
 
 blur = cv2.blur(image, (3,3))
-guassian_blur = cv2.GuassianBlur(image, (3,3), 0)
+gaussian_blur = cv2.GaussianBlur(image, (3,3), 0)
 median = cv2.medianBlur(image, 5)
+
+cv2.imshow("boxblux", blur)
+cv2.waitKey()
+cv2.imshow("gaussian", gaussian_blur)
+cv2.waitKey()
+cv2.imshow("median", median)
+cv2.waitKey()
 
 # should go look into image de-noising
 # brighten needs an array [-1,-1,-1],[-1,9,-1],[-1,-1,-1]
-matrix = np.array([],[],[])
+# kernel/convolution matrix can even be used for edge detection
+matrix = np.array([[-1,-1,-1],[-1,9,-1],[-1,-1,-1]])
+sharp = cv2.filter2D(image, -1, matrix)
+
+cv2.imshow("sharp", sharp)
+cv2.waitKey()
+cv2.destroyAllWindows()
