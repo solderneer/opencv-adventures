4e8593068d63f3996b81f7924b0b25fb88f48c73
Sudharshan <sudhar393@gmail.com>
2017-09-05 22:03:20 +0800

    gaussian blur

--- sketch-app/sketch.py
+++ sketch-app/sketch.py
@@ -6,14 +6,15 @@ cap = cv2.VideoCapture(0)
 
 def sketch(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # gray_blur = cv2.GaussianBlur(gray, (3,3), 0)
     edges = cv2.Canny(gray, 100, 200)
-    ret, thresh1 = cv2.threshold(edges, 127, 255, cv2.THRESH_BINARY_INV)
+    ret, thresh1 = cv2.threshold(edges, 20, 255, cv2.THRESH_BINARY_INV)
     return thresh1
 
 while(True):
     ret, frame = cap.read()
     cv2.imshow('sketch-app', sketch(frame))
-    if cv2.waitKey(1) == '13':
+    if cv2.waitKey(1) == 13:
         break
 
 cap.release()
