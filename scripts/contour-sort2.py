0495b7c8c6ab29fd1ef7adffb8239aabdd42b44e
Sudharshan <sudhar393@gmail.com>
2017-09-10 11:04:50 +0800

    sorting by position

--- scripts/contour-sort2.py
+++ scripts/contour-sort2.py
@@ -2,14 +2,21 @@
 import cv2
 import numpy as np
 
+def calcXpos(contour):
+    M = cv2.moments(contour)
+    return (int(M['m10']/M['m00']))
+
 image = cv2.imread('../images/shapes_donut.jpg')
 grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(grayscale, 100, 200)
-image1, contours, hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+image1, contours, hierarchy = cv2.findContours(edges,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
-image = cv2.drawContours(image, contours, -1, (0,255,0), 3)
+contours = sorted(contours, key = calcXpos)
 
-cv2.imshow('contours', image)
-cv2.imshow('edges', edges)
+for cnt in contours:
+    image = cv2.drawContours(image, cnt, -1, (0,255,0),3)
+    cv2.imshow('contours', image)
+    cv2.waitKey()
+
 cv2.waitKey()
 cv2.destroyAllWindows()
