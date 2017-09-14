88c5a4cfb7a12191415c2e4e11957bda48c4abbc
Sudharshan <sudhar393@gmail.com>
2017-09-14 19:40:38 +0800

    generated contours

--- shapematch/shapematch.py
+++ shapematch/shapematch.py
@@ -4,4 +4,11 @@ import numpy as np
 
 image = cv2.imread('../images/someshapes.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(gray, 127, 255, 0)
+img, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
+cv2.drawContours(image, contours, -1, (0,255,0), 3)
+cv2.imshow('contours', image)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
