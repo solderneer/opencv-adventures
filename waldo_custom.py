151b278da0ec1300ece524787e4c5d57aec0b139
Sudharshan <sudhar393@gmail.com>
2017-10-23 18:28:58 +0800

    implementing custom convolving function

--- waldo_custom.py
+++ waldo_custom.py
@@ -4,6 +4,12 @@ import numpy as np
 from sklearn.preprocessing import scale
 import operator
 
+def convolve2D(image_mat, filter_mat):
+    # First add boundary to image
+
+    row, column = filter_mat.shape
+    reflect = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REFLECT)
+
 image = cv2.imread('images/WaldoBeach.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow('Beach', gray)
@@ -15,15 +21,13 @@ gray_template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 cv2.imshow('Waldo', gray_template)
 cv2.waitKey()
 gray_template_normal = scale(gray_template, axis=0, with_mean=True, with_std=True, copy=True)
-gray_template_normal = np.rot90(gray_template_normal,2)
 
 dst = cv2.filter2D(gray_normal,-1,gray_template_normal)
-i,j = np.unravel_index(dst.argmax(), dst.shape)
+indices = np.where(dst == dst.max())
 
-print(i,j)
-print(dst[i,j])
+print(indices)
 
-img = cv2.circle(image,(i,j),63,(0,0,255),3)
+img = cv2.circle(image,indices,63,(0,0,255),3)
 cv2.imshow('Where is Waldo?', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
