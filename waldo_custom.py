20abea930f44296367217145fab73866ea654084
Sudharshan <sudhar393@gmail.com>
2017-10-24 18:31:08 +0800

    added explicit convolution function

--- waldo_custom.py
+++ waldo_custom.py
@@ -2,32 +2,65 @@
 import cv2
 import numpy as np
 from sklearn.preprocessing import scale
+from __future__ import divison
 import operator
 
-def convolve2D(image_mat, filter_mat):
-    # First add boundary to image
+def naive_convolve(f, g):
+    g = scale( g, axis=0, with_mean=True, with_std=True, copy=True ) # normalized filter
 
-    row, column = filter_mat.shape
-    reflect = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REFLECT)
+    # f is an image and is indexed by (v, w)
+    # g is a filter kernel and is indexed by (s, t),
+    #   it needs odd dimensions
+    # h is the output image and is indexed by (x, y),
+    #   it is not cropped
+    if g.shape[0] % 2 != 1 or g.shape[1] % 2 != 1:
+        raise ValueError("Only odd dimensions on filter supported")
+    # smid and tmid are number of pixels between the center pixel
+    # and the edge, ie for a 5x5 filter they will be 2.
+    #
+    # The output size is calculated by adding smid, tmid to each
+    # side of the dimensions of the input image.
+    vmax = f.shape[0]
+    wmax = f.shape[1]
+    smax = g.shape[0]
+    tmax = g.shape[1]
+    smid = smax // 2
+    tmid = tmax // 2
+    xmax = vmax + 2*smid
+    ymax = wmax + 2*tmid
+    # Allocate result image.
+    h = np.zeros([xmax, ymax], dtype=f.dtype)
+    # Do convolution
+    for x in range(xmax):
+        for y in range(ymax):
+            # Calculate pixel value for h at (x,y). Sum one component
+            # for each pixel (s, t) of the filter g.
+            s_from = max(smid - x, -smid)
+            s_to = min((xmax - x) - smid, smid + 1)
+            t_from = max(tmid - y, -tmid)
+            t_to = min((ymax - y) - tmid, tmid + 1)
+            value = 0
+            for s in range(s_from, s_to):
+                for t in range(t_from, t_to):
+                    v = x - smid + s
+                    w = y - tmid + t
+                    value += g[smid - s, tmid - t] * f[v, w]
+            h[x, y] = value
+    return h
 
 image = cv2.imread('images/WaldoBeach.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow('Beach', gray)
 cv2.waitKey()
-gray_normal = scale(gray, axis=0, with_mean=True, with_std=True, copy=True)
 
 template = cv2.imread('images/waldo.jpg')
 gray_template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 cv2.imshow('Waldo', gray_template)
 cv2.waitKey()
-gray_template_normal = scale(gray_template, axis=0, with_mean=True, with_std=True, copy=True)
 
-dst = cv2.filter2D(gray_normal,-1,gray_template_normal)
-indices = np.where(dst == dst.max())
+naive_convolve(gray, gray_template)
 
-print(indices)
-
-img = cv2.circle(image,indices,63,(0,0,255),3)
+img = cv2.circle(image,(23,23),63,(0,0,255),3)
 cv2.imshow('Where is Waldo?', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
